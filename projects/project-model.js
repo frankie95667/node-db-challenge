5a7e3dc5794605cc6bdd7c049d229c6f5d4658b1
Anthony Kawa <anthonyk2020@gmail.com>
2020-04-17 10:41:57 -0700

    GET endpoint for project with specific project id

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,6 +1,9 @@
 module.exports = {
     get,
-    add
+    add,
+    getById,
+    getTasks,
+    getResources
 }
 
 const db = require('../data/dbConfig');
@@ -12,6 +15,23 @@ function get(){
 function getById(id){
     return db('projects')
     .where({id}).first();
+}
+
+function getTasks(id){
+    return db("tasks as t").select("t.id", "t.description", "t.notes", "t.completed")
+    .join("projects as p", function(){
+        return this.on("p.id", "=", "t.project_id");
+    })
+    .where({"p.id": id});
+}
+
+function getResources(id){
+    return db("resources as r").select("r.id", "r.name", "r.description")
+    .join("projects as p")
+    .join("project_resources as pr", function(){
+        return this.on("pr.project_id", "=", "p.id").andOn("pr.resource_id", "=", "r.id");
+    })
+    .where({"p.id": id});
 }
 
 function add(resource){
--- projects/project-router.js
+++ projects/project-router.js
@@ -1,6 +1,9 @@
 const {
     get,
-    add
+    add,
+    getById,
+    getTasks,
+    getResources
 } = require('./project-model');
 const router = require('express').Router();
 
@@ -14,6 +17,27 @@ router.post('/', (req, res) => {
     add(req.body)
     .then(project => res.status(201).json(project))
     .catch(err => res.status(500).json({errorMessage: "Something went wrong when adding project"}))
+})
+
+router.get('/:id', (req, res) => {
+    let obj = {};
+    const {id} = req.params;
+    getById(id)
+    .then(project => {
+        obj = {...project};
+        return getTasks(id);
+    })
+    .then(tasks => {
+        obj.tasks = tasks;
+        return getResources(id);
+    })
+    .then(resources => {
+        obj.resources = resources;
+        res.status(200).json(obj);
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "Something went wrong when retrieving projects"})
+    })
 })
 
 module.exports = router;
